7aec219a685d6a9413a12d968c5375a64e51e913
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-26 13:16:24 -0400

    add return bexmp

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ func main() {
 	Greeting()
 	advanceGreeting("M")
 	fmt.Println(add(1, 2))
-	add(1, 2)
-	divide(10, 3)
+	c := add(1, 2)
+	d := 5
+	e := c + d
+	fmt.Println(e)
+	q, r := divide(10, 3)
+	// var q int
+	// q = 10
+	// var q int = 10
+	fmt.Printf("Quotient: %d, REminder: %d\n", q, r)
+	q, r = 20, 3
+	fmt.Printf("Quotient: %d, REminder: %d\n", q, r)
 
 }
